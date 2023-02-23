07421b073075d0ae737eeee092157f83752a1f03
adrianezd <adrianezquerrods@gmail.com>
2023-02-23 17:44:23 +0100

    socket io inicialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Socket.io Client</h1>
+        <button onClick={() => socket.emit('message', 'Hello World!')}>Send Message</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const { Socket } = require
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const port = 3001;
+const { Server } = require("socket.io");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+    console.log(`a user connected, ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`a user disconnected, ${socket.id}`);
+    });
+});
 
 
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+}
+);
 
+app.get("/", (req, res) => {
+    
+});
