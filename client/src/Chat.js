1d262b75809c2be443bbb3de0bcdf21c565137d4
adrianezd <adrianezquerrods@gmail.com>
2023-02-23 20:38:32 +0100

    chat.js structure

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -4,8 +4,23 @@ import Header from './Header'
 function Chat({socket, username, room}) {
   return (
     <div>
-        <Header username={username} room={room} />
-        <h1>Chat</h1>
+        <div className='chat-header'>
+            <Header username={username} room={room} />
+        </div>
+
+        <div className='chat-title'>
+            <h1>No chat in this room...</h1>
+        </div>
+
+        <div className='chat-messages'>
+            <div className='message'>
+            </div>
+        </div>
+
+        <div className='chat-send'>
+            <input type="text" placeholder="Message" />
+            <button>Send</button>
+        </div>
     </div>
   )
 }
